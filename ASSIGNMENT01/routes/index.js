6046950673fe1085daa418f12b273789af4c4c80
nupur12332 <nupurpatel30903@gmail.com>
2024-02-16 21:34:46 -0500

    comit2
    comitting the final folder to git hub

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,22 @@
+// index.js file contains all the funtionalities of the website
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/home', (req, res) => {
+router.get('/home.hbs', (req, res) => {
   res.render('home.hbs');
+});
+// get know me page
+router.get('/knowme.hbs', (req, res) => {
+  res.render('knowme.hbs');
+});
+//get projects page
+router.get('/projects.hbs', (req, res) => {
+  res.render('projects.hbs');
+});
+//get contact me page
+router.get('/contact.hbs', (req, res) => {
+  res.render('contact.hbs');
 });
 
 module.exports = router;
