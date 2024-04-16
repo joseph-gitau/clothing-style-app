ad46114fc76bcca92ffe17eafe140edd38c4517c
nupur12332 <nupurpatel30903@gmail.com>
2024-04-15 20:33:54 -0400

    assignment2
    Commit 1

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const Book = require("../models/book");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home.hbs', (req, res) => {
+  res.render('home.hbs');
+});
+// get know me page
+router.get('/signup.hbs', async(req, res,next) => {
+  let book = await Book.find()
+  res.render('signup.hbs', {dataset: book});
+});
+//get projects page
+router.get('/signin.hbs', (req, res) => {
+  res.render('signin.hbs');
+});
+//get contact me page
+router.get('/recommend.hbs', (req, res) => {
+  res.render('recommend.hbs');
 });
 
+router.post("/signup", async(req, res, next)=>{
+  let newBook = new Book({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  await newBook.save();
+  res.redirect("/signup");
+});
 module.exports = router;
